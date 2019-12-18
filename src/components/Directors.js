f523962926b7f9d5764431f9142b15d2626e3e53
iamtash <natashachernis@gmail.com>
2019-12-18 10:47:53 -0600

    Done.

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -1,10 +1,36 @@
 import React from 'react';
 import { directors } from '../data';
 
+const directorStyle = {
+  border: '1px solid black',
+  width: '500px',
+  margin: '1em',
+  padding: '1em'
+}
+
 const Directors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>
+        Directors Page
+      </h1>
+      {directors.map(({name, movies}) =>
+        <div 
+        className='director'
+        style={directorStyle}
+        >
+          <h2>
+            {name}
+          </h2>
+          <ul>
+            {movies.map(movie => 
+              <li>
+                {movie}
+              </li>
+            )}
+          </ul>
+        </div>
+      )}
     </div>
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,47 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
+const link = {
+  width: '100px',
+  padding: '12px',
+  margin: '0 6px 6px',
+}
+
 const NavBar = () => {
   return (
-    <div>
-      {/*{code here}*/}
+    <div className='navbar'>
+      <NavLink 
+        to='/' 
+        exact 
+        style={link} 
+        activeStyle={{
+          color: 'black'
+        }} 
+        >Home</NavLink>
+      <NavLink 
+        to='/movies' 
+        exact 
+        style={link} 
+        activeStyle={{
+          color: 'black'
+        }} 
+        >Movies</NavLink>
+      <NavLink 
+        to='/directors' 
+        exact 
+        style={link} 
+        activeStyle={{
+          color: 'black'
+        }} 
+        >Directors</NavLink>
+      <NavLink 
+        to='/actors' 
+        exact 
+        style={link} 
+        activeStyle={{
+          color: 'black'
+        }} 
+        >Actors</NavLink>
     </div>
   );
 };
