224dea459e3eb22161547f161d03caa81a61c1a3
Michael Schiumo <mschiumo18@gmail.com>
2019-12-31 15:18:25 -0500

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,8 +4,8 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        <h1>Movies Page</h1>
-        {console.log(props)}
+      <h1>Movies Page</h1>
+        {movies && movies.map(movie => <div className="movies">{movie.title} {movie.time} <li>{movie.genres}</li></div>)}
     </div>
   );
 };
